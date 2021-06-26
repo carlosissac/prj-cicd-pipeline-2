9e6d327ed759bcf4d80d2977f4eecf2739f4f8f1
Issac Angulo <Issac.Angulo@wholefoods.com>
2021-06-25 22:44:52 -0500

    initial commit

---
+++ src/main/java/com/project/bookzy/controllers/WelcomePageController.java
@@ -0,0 +1,13 @@
+package com.project.bookzy.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomePageController {
+  @GetMapping("/")
+  public String showWelcomeMessage() {
+  String returnMessage = "Hello ...... Welcome to Bookzy App!!!!";
+  return returnMessage;
+  } 
+}
